4b9503c8bd1601ee5254efd3b5097eab1d31212e
Balaji Kannan <bkannan@opensourceintegrators.com>
2026-06-17 16:54:58 +0000

    globals dict validation issues fixed in migration.

--- base_tier_validation_formula/__manifest__.py
+++ base_tier_validation_formula/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Base Tier Validation Formula",
     "summary": """
         Formulas for Base tier validation""",
-    "version": "19.0.1.0.1",
+    "version": "19.0.1.0.2",
     "category": "Tools",
     "license": "AGPL-3",
     "development_status": "Mature",
